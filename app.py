57da99ee7c8db9faf87c95559f91d0a3c83f5e78
ipshitasingh <ipshita0501@gmail.com>
2024-01-15 00:48:22 +0530

    added tests

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 def hello():
    return "Hello World!"
 
+#defining the /other route 
+@app.route("/other")  
+def other_route():
+    return "This is another route"
+
 #to run the app when this script is executed
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8080)
-
